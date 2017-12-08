f6a6558aa2f8eb7c450ee96e7ee8e1a73fb2da9a
liugenjie <liugenjie@gmail.com>
2017-12-08 20:24:42 +0800

    add binary sort tree

---
+++ File.h
@@ -0,0 +1,14 @@
+//
+//  File.h
+//  Data Structures
+//
+//  Created by 刘根杰 on 08/12/2017.
+//  Copyright © 2017 刘根杰. All rights reserved.
+//
+
+#ifndef File_h
+#define File_h
+
+#include <stdio.h>
+
+#endif /* File_h */
---
+++ Sort.h
@@ -0,0 +1,31 @@
+//
+//  Sort.h
+//  Data Structures
+//
+//  Created by 刘根杰 on 08/12/2017.
+//  Copyright © 2017 刘根杰. All rights reserved.
+//
+
+#ifndef Sort_h
+#define Sort_h
+
+#include <stdio.h>
+
+// 插入排序法
+
+// 折半插入排序法
+
+// 选择排序法
+
+// 泡排序法
+
+// 谢尔排序法
+
+// 快速排序法
+
+// 堆积排序法
+
+// 二路归并排序法 略
+
+
+#endif /* Sort_h */
