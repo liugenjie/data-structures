b12087e4d698cdbfe22e434ae57e620d001d4ffd
System Administrator <liugenjie@gmail.com>
2018-03-17 17:25:29 +0800

    add file

---
+++ Graph.h
@@ -0,0 +1,14 @@
+//
+//  Graph.h
+//  Data Structures
+//
+//  Created by 刘根杰 on 15/03/2018.
+//  Copyright © 2018 刘根杰. All rights reserved.
+//
+
+#ifndef Graph_h
+#define Graph_h
+
+#include <stdio.h>
+
+#endif /* Graph_h */
---
+++ Search.h
@@ -0,0 +1,14 @@
+//
+//  Search.h
+//  Data Structures
+//
+//  Created by 刘根杰 on 17/03/2018.
+//  Copyright © 2018 刘根杰. All rights reserved.
+//
+
+#ifndef Search_h
+#define Search_h
+
+#include <stdio.h>
+
+#endif /* Search_h */
